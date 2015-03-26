ad6770ac1cf979cdcbf00acd619b02ddc4286e64
unknown <NagaPrathiba.Chinta@ATMECSINLT-010.ATMECS.CORP>
2015-03-26 11:33:30 +0530

    testsmg

---
+++ src/sampleproj/TestSample.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sampleproj;
+
+/**
+ * @author NagaPrathiba.Chinta
+ *
+ */
+public class TestSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("added!!!");
+
+	}
+
+}
